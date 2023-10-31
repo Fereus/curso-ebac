2d9b48c946841eb3df69308d20b228bbfe530344
Fernando <fernando_20048@hotmail.com>
2023-10-31 15:34:40 -0300

    tarefa 8

---
+++ mod8/tarefa_calculo_media/src/Main.java
@@ -0,0 +1,18 @@
+import br.com.fernando.Media;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Media media = new Media();
+        double nota1 = 7;
+        double nota2 = 6 ;
+        double nota3 = 1;
+        double nota4 = 9;
+
+        media.calculoMedia(nota1, nota2, nota3, nota4);
+        //  System.out.printf("Hello and welcome!");
+
+
+    }
+}
---
+++ mod8/tarefa_calculo_media/src/br/com/fernando/Media.java
@@ -0,0 +1,9 @@
+package br.com.fernando;
+
+public class Media {
+
+    public void calculoMedia(double nota1, double nota2, double nota3, double nota4) {
+        double media = (nota1 + nota2 + nota3 + nota4)/ 4;
+        System.out.println(media);
+    }
+}
