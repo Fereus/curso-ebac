3d5eae17ed0f87f2beb730a5950b336b04e8e615
Fernando <fernando_20048@hotmail.com>
2023-10-31 16:14:47 -0300

    tarefa 9

---
+++ mod9/tarefa_wrapper/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        System.out.println(num);
+
+        num = Integer.valueOf(100);
+
+        System.out.println(num);
+    }
+}
